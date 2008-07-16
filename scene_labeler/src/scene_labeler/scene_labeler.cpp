9ce1c65f4bf682b9b9e4d20a4c58b1a43b5b6637
Alex Teichman <alexteichman@willowgarage.com>
2008-07-16 01:32:06 +0000

    scene_labeler can now load logged pointclouds and run scanutils functions on them.
    git-svn-id: https://code.ros.org/svn/ros-pkg/pkg/trunk/visualization@1664 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- scene_labeler/src/scene_labeler/scene_labeler.cpp
+++ scene_labeler/src/scene_labeler/scene_labeler.cpp
@@ -2,6 +2,7 @@
 #include <ros/node.h>
 #include <std_msgs/Empty.h>
 #include <logsnarf/logsnarf.h>
+#include <smartScan.h>
 
 using namespace std;
 
@@ -27,6 +28,8 @@ public:
   }
 
 
+  
+
   LogPlayer<std_msgs::PointCloudFloat32> cloud;
   LogSnarfer s;
   std_msgs::Empty shutter;
@@ -35,8 +38,6 @@ public:
 
 
 int main(int argc, char **argv) {
-  const int SLEEPTIME = 100000;
-
   int x=0;
   ros::init(x, NULL);
   usleep(500000);
@@ -46,7 +47,51 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  //  Load the data.
+  scene_labeler sl;
+  sl.read_bag(argv[1]);
 
+  //Put it into scan_utils.
+  SmartScan *ss = new SmartScan();
+  ss->setPoints(sl.cloud.get_pts_size(), sl.cloud.pts);
+  ss->crop(0,0,0,2,2,2);
+  ss->removeGrazingPoints(1);
+  vector<std_msgs::Point3DFloat32> *ptsInRadius = ss->getPointsWithinRadius(0, 0, 0, 10);
+
+  
+  //Load a message with the modified cloud and publish.
+  std_msgs::PointCloudFloat32 c2;
+  c2.set_pts_size(ptsInRadius->size());
+  c2.set_chan_size(1);
+  c2.chan[0].name = "intensities";
+  c2.chan[0].set_vals_size(ptsInRadius->size());
+  for(unsigned int i=0; i<ptsInRadius->size(); i++) {
+    c2.pts[i].x = (*ptsInRadius)[i].x;
+    c2.pts[i].y = (*ptsInRadius)[i].y;
+    c2.pts[i].z = (*ptsInRadius)[i].z;
+    c2.chan[0].vals[i] = 0;
+  }
+  sl.publish("full_cloud", c2);
+  cout << "Published cloud." << endl;
+
+  
+
+  delete ss;
+  cout << "deleting" << endl;
+
+  //usleep(5000000);
+  usleep(500000);
+  ros::fini();
+  return 0;
+}
+
+
+
+
+
+
+/* Old debugging crap.
+  const int SLEEPTIME = 100000;
   //This does not work.
 //   scene_labeler lab;
 //   lab.publish("shutter", lab.shutter);
@@ -86,9 +131,4 @@ int main(int argc, char **argv) {
 //     lab.publish("full_cloud", lab.cloud);
 //     usleep(SLEEPTIME);
 //   }
-  
-
-  usleep(500000);
-  ros::fini();
-  return 0;
-}
+*/
