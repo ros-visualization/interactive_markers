68bd8ea5d313d9b1186d94ea5b9d607f63f5c6a8
Alex Teichman <alexteichman@willowgarage.com>
2008-07-19 00:40:30 +0000

    Making scene_labeler have separate segmentation and labeling modes, since we will probably want to do the segmentation offline.
    git-svn-id: https://code.ros.org/svn/ros-pkg/pkg/trunk/visualization@1796 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- scene_labeler/src/scene_labeler/scene_labeler.cpp
+++ scene_labeler/src/scene_labeler/scene_labeler.cpp
@@ -5,9 +5,21 @@
 #include <std_msgs/Empty.h>
 #include <logsnarf/logsnarf.h>
 #include <smartScan.h>
+#include <time.h>
 
 using namespace std;
 
+#define MINPTS = 100;
+#define CCTHRESH = .01;
+
+
+void SplitFilename (const string& str, string *dir, string *file)
+{
+  size_t found;
+  found=str.find_last_of("/\\");
+  *file = str.substr(found+1);
+  *dir = str.substr(0,found);
+}
 
 void emptycallback(ros::Time t) {
 }
@@ -22,8 +34,6 @@ public:
 
     cloud[0] = new LogPlayer<std_msgs::PointCloudFloat32>();
     cloud[1] = new LogPlayer<std_msgs::PointCloudFloat32>();
-//     cloud.push_back(emptycloud1);
-//     cloud.push_back(emptycloud2);
 }
 
   void broadcast_pointcloud(char *filename) {
@@ -46,19 +56,19 @@ int main(int argc, char **argv) {
   ros::init(x, NULL);
   usleep(500000);
 
-
-  if(argc!=2 && argc!=3) {
-    cerr << "usage: \t scene_labeler bagfile #publishes messages in bagfile.\n"
-            "\t\t scene_labeler bagfile1 bagfile2 #subtracts bagfile2 from bagfile1 and publishes." << endl;
-    return 1;
-  }
-
   scene_labeler sl;
+
+  //Filename parsing on foreground.  Not for argc==2 case.
+  string foregroundString = string(argv[2]);
+  string fgDir, fgFile;
+  SplitFilename(foregroundString, &fgDir, &fgFile);
+  string fgNum = fgFile.substr(0, fgFile.find_first_of("-"));
+
+
+  //  If we just get one bag, load the data and publish it.
   if(argc==2) { 
-    //  Load the data.
     sl.read_bag(argv[1], 0);
     sl.publish("full_cloud", *sl.cloud[0]);
-
 
 //     //Put it into scan_utils.
 //     SmartScan *ss = new SmartScan();
@@ -84,32 +94,93 @@ int main(int argc, char **argv) {
 //     cout << "Published cloud." << endl;
 //     delete ss;
   }
-  else if(argc==3) {
+  
+  //If we get 2 bags, subtract one from the other and publish the difference.
+  else if(argc==4 && !strcmp(argv[1], "-s")) {
+    cout << "Segmenting..." << endl;
+
     //Load the data.
     sl.read_bag(argv[1], 0);
     sl.read_bag(argv[2], 1);
     
-    //Put them both into SmartScans and subtract the background.
+    //Put them both into SmartScans.
     SmartScan foreground, background;
     foreground.setPoints(sl.cloud[0]->get_pts_size(), sl.cloud[0]->pts);
     background.setPoints(sl.cloud[1]->get_pts_size(), sl.cloud[1]->pts);
-    foreground.crop(0,0,0,1.2,1.2,1.2);
-    background.crop(0,0,0,1.2,1.2,1.2);
+    //foreground.crop(0,0,0,1.2,1.2,1.2);
+    //background.crop(0,0,0,1.2,1.2,1.2);
+
+    //Subtract the background.
     cout << "starting bg subtr" << endl;
-    foreground.subtractScan(&background, .01);
+    time_t start,end;
+    time(&start);
+    foreground.subtractScan(&background, CCTHRESH);
+    time(&end); double dif = difftime(end,start);
+    cout << "Subtraction took " << dif << " seconds." << endl;
+
     foreground.removeGrazingPoints(10);
     vector<SmartScan*> *ccs;
-    ccs = foreground.connectedComponents(.01, 100);
+    ccs = foreground.connectedComponents(CCTHRESH, MINPTS);
 
+
+    //Put just the large-enough connected components into a final scan.
     SmartScan final;
     for(unsigned int i=0; i<ccs->size(); i++) {
       final.addScan((*ccs)[i]);
     }
 
-    //Publish foreground.
+    //Publish the final scan and save it to a file.
     std_msgs::PointCloudFloat32 finalcloudmsg = final.getPointCloud();
-    sl.publish("full_cloud", finalcloudmsg);
+    string toSave = fgDir + string("/") + fgNum + string("-sg-full_cloud.bag"); //TODO: make this general.
+    dustbuster *db = new dustbuster(string("full_cloud"), fgDir, string("sg"), toSave); //TODO: add a new dustbuster constructor.
+    sl.publish("full_cloud", finalcloudmsg); //This will be saved by dustbuster.
+    delete db;
   }
+
+  else if(argc==4 && !strcmp(argv[1], "-l")) {
+    cout << "Labeling..." << endl;
+
+    //Do filename parsing to get the segmented filename.
+    string segmentedString = string(argv[3]);
+    string sgDir, sgFile;
+    SplitFilename(segmentedString, &sgDir, &sgFile);
+    string sgNum = sgFile.substr(0, sgFile.find_first_of("-"));
+    if(sgNum.compare(fgNum)) {
+      cout << "Foreground and segmented file do not have the same number!" << endl;
+    }
+    string labeledFile = fgDir + string("/") + fgNum + string("-segmented.bag");
+    cout << "saving to " << labeledFile << endl;
+   
+    //Load the data.
+    sl.read_bag(argv[2], 0);
+    sl.read_bag(argv[3], 1);
+    
+    //Put them both into SmartScans.
+    SmartScan foreground, segmented;
+    foreground.setPoints(sl.cloud[0]->get_pts_size(), sl.cloud[0]->pts);
+    segmented.setPoints(sl.cloud[1]->get_pts_size(), sl.cloud[1]->pts);
+
+    //Get the segments.
+    vector<SmartScan*> *ccs;
+    ccs = foreground.connectedComponents(CCTHRESH, MINPTS);
+
+    //Present each segment in turn and ask the user for a label.
+    for(unsigned int i=0; i<ccs->size(); i++) {
+      std_msgs::PointCloudFloat32 msg = ccs[i].getPointCloud();
+      sl.publish("full_cloud", msg);
+      cout << "Pausing..." << endl;
+      int dummy; cin >> dummy; //TODO: Actually ask for and apply a label.
+    }
+  }
+
+  else {
+    cout << "usage:\n"
+            "scene_labeler bagfile \t\t\t#publishes messages in bagfile.\n"
+            "scene_labeler -s foreground background \t#subtracts background from foreground, publishes, and saves in bagfile1's directory to (number)-segmented.bag\n"
+            "scene_labeler -l foreground segmented \t#Asks user for labels of connected components in segmented, \n"
+            "\t\t\t\t\t\t  then applies labels to points in foreground and saves (number)-labeled.bag\n" << endl;
+  }
+
     
   cout << "Finished successfully!" << endl;
 
@@ -117,6 +188,7 @@ int main(int argc, char **argv) {
   ros::fini();
   return 0;
 }
+
 
 
 
