6cc10838d3dbe341a3729d5f39b00ecd250253c3
Josh Faust <jfaustwg@willowgarage.com>
2009-01-06 21:35:49 +0000

    Add code to check for/use the correct wx version
    git-svn-id: https://code.ros.org/svn/ros-pkg/pkg/trunk/visualization@8908 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- wx_rosout/scripts/standalone_rosout.py
+++ wx_rosout/scripts/standalone_rosout.py
@@ -3,6 +3,14 @@
 import rostools
 rostools.update_path('wx_rosout')
 
+WXVER = '2.8'
+import wxversion
+if wxversion.checkInstalled(WXVER):
+  wxversion.select(WXVER)
+else:
+  print >> sys.stderr, "This application requires wxPython version %s"%(WXVER)
+  sys.exit(1)
+
 import wx
 import wx_rosout
 
