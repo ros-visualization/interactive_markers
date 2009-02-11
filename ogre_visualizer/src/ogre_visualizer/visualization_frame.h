ae419e46f9203699ae9eaf92b83c538b5218eee3
Josh Faust <jfaustwg@willowgarage.com>
2009-02-11 22:20:52 +0000

    C++ equivalent of standalone_visualizer.py
    git-svn-id: https://code.ros.org/svn/ros-pkg/pkg/trunk/visualization@11006 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

---
+++ ogre_visualizer/src/ogre_visualizer/visualization_frame.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef OGRE_VISUALIZER_VISUALIZATION_FRAME_H
+#define OGRE_VISUALIZER_VISUALIZATION_FRAME_H
+
+#include <wx/wx.h>
+
+#include <string>
+
+class wxConfigBase;
+class wxMenuBar;
+class wxMenu;
+
+namespace ogre_vis
+{
+
+class VisualizationPanel;
+
+class VisualizationFrame : public wxFrame
+{
+public:
+  VisualizationFrame(wxWindow* parent);
+  ~VisualizationFrame();
+
+  void init();
+
+protected:
+  void initConfig();
+  void initMenu();
+  void loadConfig(const std::string& path);
+  void loadConfigMenus();
+  void saveConfig();
+
+  void onClose(wxCommandEvent& event);
+  void onOpen(wxCommandEvent& event);
+  void onSave(wxCommandEvent& event);
+  void onGlobalConfig(wxCommandEvent& event);
+  void onLocalConfig(wxCommandEvent& event);
+
+  VisualizationPanel* visualization_panel_;
+
+  wxConfigBase* config_;
+  std::string config_dir_;
+  std::string config_file_;
+  std::string save_dir_;
+  std::string global_config_dir_;
+
+  wxMenuBar* menubar_;
+  wxMenu* file_menu_;
+  wxMenu* local_configs_menu_;
+  wxMenu* global_configs_menu_;
+
+};
+
+}
+
+#endif // OGRE_VISUALIZER_VISUALIZATION_FRAME_H
