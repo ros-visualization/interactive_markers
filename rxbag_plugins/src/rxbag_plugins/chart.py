753ff12140285baba6842723f819f749d5c52e8f
Tim Field <tfield@willowgarage.com>
2010-11-05 18:51:28 +0000

    rxbag_plugins: merging fixes for #4415, #3028 from trunk into vvisualization-1.2
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/branches/visualization-1.2@33723 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- rxbag_plugins/src/rxbag_plugins/chart.py
+++ rxbag_plugins/src/rxbag_plugins/chart.py
@@ -30,6 +30,8 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import division
+
 PKG = 'rxbag_plugins'
 import roslib; roslib.load_manifest(PKG)
 
