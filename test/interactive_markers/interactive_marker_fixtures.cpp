3d0626796aff859f39c68ffe75e5fc26128dfa10
Chris Lalancette <clalancette@openrobotics.org>
2022-05-02 15:16:10 +0000

    Move get_interactive_markers to a cpp file.
    That way it could be included more than once.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- test/interactive_markers/interactive_marker_fixtures.cpp
+++ test/interactive_markers/interactive_marker_fixtures.cpp
@@ -26,16 +26,43 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef INTERACTIVE_MARKERS__INTERACTIVE_MARKER_FIXTURES_HPP_
-#define INTERACTIVE_MARKERS__INTERACTIVE_MARKER_FIXTURES_HPP_
-
 #include <vector>
 
 #include "visualization_msgs/msg/interactive_marker.hpp"
+#include "visualization_msgs/msg/interactive_marker_control.hpp"
+#include "visualization_msgs/msg/menu_entry.hpp"
 
-#include "interactive_markers/visibility_control.hpp"
+#include "interactive_marker_fixtures.hpp"
 
-INTERACTIVE_MARKERS_PUBLIC
-std::vector<visualization_msgs::msg::InteractiveMarker> get_interactive_markers();
-
-#endif  // INTERACTIVE_MARKERS__INTERACTIVE_MARKER_FIXTURES_HPP_
+std::vector<visualization_msgs::msg::InteractiveMarker> get_interactive_markers()
+{
+  std::vector<visualization_msgs::msg::InteractiveMarker> markers;
+  {
+    visualization_msgs::msg::InteractiveMarker marker;
+    marker.name = "test_marker_0";
+    marker.header.frame_id = "test_frame_id";
+    markers.push_back(marker);
+  }
+  {
+    visualization_msgs::msg::InteractiveMarker marker;
+    marker.name = "test_marker_1";
+    marker.header.frame_id = "test_frame_id";
+    marker.pose.position.x = 1.0;
+    marker.pose.orientation.w = 1.0;
+    marker.description = "My test marker description";
+    marker.scale = 3.14f;
+    visualization_msgs::msg::MenuEntry menu_entry;
+    menu_entry.id = 42;
+    menu_entry.title = "My test menu title";
+    menu_entry.command = "Some test command to be run";
+    marker.menu_entries.push_back(menu_entry);
+    visualization_msgs::msg::InteractiveMarkerControl control;
+    control.name = "test_control_name";
+    control.orientation.w = 1.0;
+    control.always_visible = true;
+    control.description = "My test control description";
+    marker.controls.push_back(control);
+    markers.push_back(marker);
+  }
+  return markers;
+}
