b02fcb706418a0dfeaaa13b88d101e9c23094134
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-05 15:27:20 +0200

    Use non deprecated API
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- src/interactive_marker_client.cpp
+++ src/interactive_marker_client.cpp
@@ -76,7 +76,7 @@ void InteractiveMarkerClient::connect(const std::string & topic_namespace)
       graph_interface_,
       services_interface_,
       topic_namespace_ + "/get_interactive_markers",
-      rmw_qos_profile_services_default,
+      rclcpp::ServicesQoS(),
       nullptr);
 
     feedback_pub_ = rclcpp::create_publisher<visualization_msgs::msg::InteractiveMarkerFeedback>(
